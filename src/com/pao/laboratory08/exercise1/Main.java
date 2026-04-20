1442b948694d8f6fe3dba1dd7f19eac092c93e89
andrachiritoiu <chiritoiu.andra@yahoo.com>
2026-04-20 10:41:16 +0300

    Rezolvare lab8

--- src/com/pao/laboratory08/exercise1/Main.java
+++ src/com/pao/laboratory08/exercise1/Main.java
@@ -17,6 +17,86 @@ public class Main {
         //    - SHALLOW <nume> → shallow clone + modifică orașul clonei la "MODIFICAT" + afișează
         //    - DEEP <nume> → deep clone + modifică orașul clonei la "MODIFICAT" + afișează
 
-        System.out.println("TODO: implementează exercițiul 1");
+        //A
+        BufferedReader br = new BufferedReader(new FileReader(FILE_PATH));
+
+        List<Student> studenti = new ArrayList<>();
+
+        String linie = br.readLine() ;
+        while(linie != null){
+            if (linie.trim().isEmpty()) {
+                linie = br.readLine();
+                continue;
+            }
+
+            String[] info = linie.split(",");
+
+            if (info.length < 4) {
+                linie = br.readLine();
+                continue;
+            }
+
+            String nume = info[0].trim();
+            int varsta = Integer.parseInt(info[1].trim());
+            String oras = info[2].trim();
+            String strada = info[3].trim();
+
+            Adresa adresa = new Adresa(oras, strada);
+            Student student = new Student(nume, varsta, adresa);
+
+            studenti.add(student);
+
+            linie = br.readLine();
+        }
+
+        br.close();
+
+        Scanner sc = new Scanner(System.in);
+        String comanda = sc.nextLine();
+
+        String[] parti = comanda.split(" ",2);
+        String tipComanda = parti[0];
+
+        if (tipComanda.equals("PRINT")) {
+            for (Student s : studenti) {
+                System.out.println(s);
+            }
+        //B
+        } else if(tipComanda.equals("SHALLOW")){
+            String numeCautat = parti[1];
+            Student studentCautat = null;
+
+            for(Student s : studenti){
+                if(s.getNume().equals(numeCautat)){
+                    studentCautat = s;
+                    break;
+                }
+            }
+
+            Student clona = (Student) studentCautat.clone();
+            clona.getAdresa().setOras("MODIFICAT");
+
+            System.out.println("Original: " + studentCautat);
+            System.out.println("Clona: " + clona);
+        }
+        //C
+        else if (tipComanda.equals("DEEP")) {
+            String numeCautat = parti[1];
+            Student studentCautat = null;
+
+            for(Student s : studenti){
+                if(s.getNume().equals(numeCautat)){
+                    studentCautat = s;
+                    break;
+                }
+            }
+
+            Student clona = (Student) studentCautat.deepClone();
+            clona.getAdresa().setOras("MODIFICAT");
+
+            System.out.println("Original: " + studentCautat);
+            System.out.println("Clona: " + clona);
+        }
+
     }
 }
