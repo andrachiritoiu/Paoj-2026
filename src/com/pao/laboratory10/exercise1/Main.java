1507fb5878c3ecad968536339579d7aad10f25aa
andrachiritoiu <chiritoiu.andra@yahoo.com>
2026-05-04 16:56:32 +0300

    Rezolvare lab10

--- src/com/pao/laboratory10/exercise1/Main.java
+++ src/com/pao/laboratory10/exercise1/Main.java
@@ -25,6 +25,114 @@ public class Main {
         // Format linie tranzacție: [id] data tip: suma RON
         //   Ex: [1] 2024-01-10 CREDIT: 500.00 RON
 
-        System.out.println("TODO: implementează exercițiul 1");
+
+        LinkedList<Tranzactie> tranzactii = new LinkedList<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()){
+            String comanda = scanner.next();
+
+            switch (comanda){
+                //A
+                case "ENQUEUE":{
+                    int id = scanner.nextInt();
+                    double suma = scanner.nextDouble();
+                    String data = scanner.next();
+                    TipTranzactie tip = TipTranzactie.valueOf(scanner.next());
+
+                    Tranzactie tranzactie = new Tranzactie(id, suma, data, tip);
+                    tranzactii.addLast(tranzactie);
+                    break;
+                }
+
+                case "DEQUEUE":{
+                    if(tranzactii.isEmpty()){
+                        System.out.println("Coada goala.");
+                    }
+                    else{
+                        Tranzactie tranzactie = tranzactii.removeFirst();
+                        System.out.println("Procesat: " + tranzactie);
+                    }
+
+                    break;
+                }
+
+                case "PUSH":{
+                    int id = scanner.nextInt();
+                    double suma = scanner.nextDouble();
+                    String data = scanner.next();
+                    TipTranzactie tip = TipTranzactie.valueOf(scanner.next());
+
+                    Tranzactie tranzactie = new Tranzactie(id, suma, data, tip);
+                    tranzactii.addFirst(tranzactie);
+                    break;
+                }
+
+                case "POP":{
+                    if(tranzactii.isEmpty()){
+                        System.out.println("Coada goala.");
+                    }
+                    else{
+                        Tranzactie tranzactie = tranzactii.removeFirst();
+                        System.out.println("Extras: " + tranzactie);
+                    }
+
+                    break;
+                }
+
+                case "PRINT":{
+                    for(Tranzactie tranzactie : tranzactii){
+                        System.out.println(tranzactie);
+                    }
+
+                    break;
+                }
+
+                case "SIZE":{
+                    System.out.println("Dimensiune coada: " + tranzactii.size());
+                    break;
+                }
+
+                //B
+                case "REMOVE_DEBIT":{
+                    Iterator<Tranzactie> itr = tranzactii.iterator();
+
+                    int cnt = 0;
+                    while(itr.hasNext()){
+                        Tranzactie t = itr.next();
+
+                        if(t.getTip() == TipTranzactie.DEBIT){
+                            itr.remove();
+                            cnt++;
+                        }
+                    }
+                    System.out.println("Eliminat " + cnt + " tranzactii DEBIT.");
+
+                    break;
+                }
+
+                case "REMOVE_BELOW":{
+                    double threshold = scanner.nextDouble();
+
+                    Iterator<Tranzactie> itr = tranzactii.iterator();
+
+                    int cnt = 0;
+                    while(itr.hasNext()){
+                        Tranzactie t = itr.next();
+
+                        if(t.getSuma() < threshold){
+                            itr.remove();
+                            cnt++;
+                        }
+                    }
+                    System.out.printf("Eliminat %d tranzactii sub %.2f RON.%n", cnt, threshold);
+
+                    break;
+                }
+            }
+        }
+
+        scanner.close();
     }
 }
--- src/com/pao/laboratory10/exercise2/Main.java
+++ src/com/pao/laboratory10/exercise2/Main.java
@@ -1,8 +1,8 @@
 package com.pao.laboratory10.exercise2;
-
 import com.pao.laboratory10.exercise1.Tranzactie;
 import com.pao.laboratory10.exercise1.TipTranzactie;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
@@ -37,6 +37,151 @@ public class Main {
         // Format linie tranzacție: [id] data tip: suma RON
         //   Ex: [1] 2024-01-15 CREDIT: 1500.00 RON
 
-        System.out.println("TODO: implementează exercițiul 2");
+
+        //1
+        ArrayList<Tranzactie> tranzactii = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i=0; i<n; i++){
+            int id = scanner.nextInt();
+            double suma = scanner.nextDouble();
+            String data = scanner.next();
+            TipTranzactie tip = TipTranzactie.valueOf(scanner.next());
+
+            Tranzactie tranzactie = new Tranzactie(id, suma, data, tip);
+            tranzactii.addLast(tranzactie);
+        }
+
+
+        //2
+        while(scanner.hasNext()) {
+            String comanda = scanner.next();
+
+            switch (comanda) {
+                case "UNIQUE_IDS": {
+                    LinkedHashSet<Integer> idsU = new LinkedHashSet<>();
+
+                    for(Tranzactie tranzactie : tranzactii){
+                        idsU.add(tranzactie.getId());
+                    }
+
+                    System.out.println("IDs unice (" + idsU.size() + "): " + idsU);
+
+                    break;
+                }
+
+                case "MONTHLY_REPORT": {
+                    TreeMap<String, double[]> raport = new TreeMap<>();
+
+                    for(Tranzactie tranzactie : tranzactii){
+                        String data = tranzactie.getData().substring(0,7);
+
+                        if (!raport.containsKey(data)){
+                            raport.put(data, new double[]{0.0, 0.0});
+                        }
+
+                        if(tranzactie.getTip() == TipTranzactie.CREDIT){
+                            raport.get(data)[0] += tranzactie.getSuma();
+                        }
+                        else if(tranzactie.getTip() == TipTranzactie.DEBIT){
+                            raport.get(data)[1] += tranzactie.getSuma();
+                        }
+
+                    }
+
+                    for (Map.Entry<String, double[]> entry : raport.entrySet()) {
+                        String luna = entry.getKey();
+                        double credit = entry.getValue()[0];
+                        double debit = entry.getValue()[1];
+
+                        System.out.printf("%s: CREDIT %.2f RON, DEBIT %.2f RON%n", luna, credit, debit);
+                    }
+
+                    break;
+                }
+
+                case "TOP": {
+                    int topN = scanner.nextInt();
+
+                    ArrayList<Tranzactie> copie = new ArrayList<>(tranzactii);
+
+                    Collections.sort(copie, Comparator.comparing(Tranzactie::getSuma).reversed());
+
+
+                    System.out.println("Top " + topN + ":");
+
+                    for(int i=0; i<topN; i++){
+                        System.out.println(copie.get(i));
+                    }
+
+                    break;
+                }
+
+                case "SORT_ASC": {
+                    Collections.sort(tranzactii, Comparator.comparing(Tranzactie::getSuma));
+
+                    for(Tranzactie tranzactie : tranzactii){
+                        System.out.println(tranzactie);
+                    }
+
+                    break;
+                }
+
+                case "SORT_DESC": {
+                    Collections.sort(tranzactii, Comparator.comparing(Tranzactie::getSuma).reversed());
+
+                    for(Tranzactie tranzactie : tranzactii){
+                        System.out.println(tranzactie);
+                    }
+
+                    break;
+                }
+
+                case "REVERSE": {
+                    Collections.reverse(tranzactii);
+
+                    for(Tranzactie tranzactie : tranzactii){
+                        System.out.println(tranzactie);
+                    }
+
+                    break;
+                }
+
+                case "MIN_MAX": {
+                    Tranzactie min = tranzactii.get(0);
+                    Tranzactie max = tranzactii.get(0);
+
+                    for(Tranzactie tranzactie : tranzactii){
+                        if(tranzactie.getSuma() < min.getSuma()){
+                            min = tranzactie;
+                        }
+                        else if(tranzactie.getSuma() > max.getSuma()){
+                            max = tranzactie;
+                        }
+                    }
+
+                    System.out.println("MIN: " + min);
+                    System.out.println("MAX: " + max);
+
+                    break;
+
+                }
+                case "CME_DEMO": {
+                    try {
+                        for (Tranzactie t : tranzactii) {
+                            tranzactii.remove(t);
+                        }
+                    } catch (ConcurrentModificationException e) {
+                        System.out.println("ConcurrentModificationException prins: modificare in iteratie detectata.");
+                    }
+
+                    break;
+                }
+
+            }
+        }
+        scanner.close();
     }
 }
